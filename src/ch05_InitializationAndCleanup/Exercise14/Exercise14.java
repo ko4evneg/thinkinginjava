919ada499331000ca504587fb4aa3edb7cb530df
ko4evneg <ko4evneg@gmail.com>
2021-02-01 23:16:53 +0300

    Ch05 ex 13-14 commit

---
+++ src/ch05_InitializationAndCleanup/Exercise14/Exercise14.java
@@ -0,0 +1,28 @@
+package ch05_InitializationAndCleanup.Exercise14;
+//Exercise 14: (1) Create a class with a static String field that is initialized at the point of definition,
+// and another one that is initialized by the static block. Add a static method that prints both fields and
+// demonstrates that they are both initialized before they are used.
+
+import static thinkinginjava.Utils.print;
+
+public class Exercise14 {
+    public static void main(String[] args) {
+        Test.outPrint();
+    }
+}
+
+class Test {
+    static String str = "lalaString";
+    static String s;
+
+    static {
+        s = "staticblockString";
+    }
+
+    static void outPrint() {
+        print("Init start");
+        print(str);
+        print(s);
+        print("Init end");
+    }
+}
