b337c3872c9977e5f5b3de3fb7de3a4f2c31e269
ko4evneg <ko4evneg@gmail.com>
2021-02-03 19:17:17 +0300

    Ch05 ex 19 - 20

---
+++ src/ch05_InitializationAndCleanup/Exercise19/Exercise19.java
@@ -0,0 +1,25 @@
+package ch05_InitializationAndCleanup.Exercise19;
+//Exercise 19: (2) Write a method that takes a vararg String array. Verify that you can pass either a
+//comma-separated list of Strings or a String[] into this method.
+
+import static thinkinginjava.Utils.print;
+
+public class Exercise19 {
+    public static void main(String[] args) {
+        String[] strs = new String[5];
+        for (int i = 1; i <= 5; i++) {
+            String s = String.valueOf(i);
+            strs[i - 1] = s;
+        }
+
+        stringer(strs);
+        print("------------");
+        stringer("a","b","c","d");
+    }
+
+    static void stringer(String...strs) {
+        for (String s : strs) {
+            print(s);
+        }
+    }
+}
---
+++ src/ch05_InitializationAndCleanup/Exercise20/Exercise20.java
@@ -0,0 +1,13 @@
+package ch05_InitializationAndCleanup.Exercise20;
+//Exercise 20: (1) Create a main( ) that uses varargs instead of the ordinary main( ) syntax. Print
+//all the elements in the resulting args array. Test it with various numbers of command-line arguments.
+
+import static thinkinginjava.Utils.print;
+
+public class Exercise20 {
+    public static void main(String...args) {
+        for (String s : args) {
+            print(s);
+        }
+    }
+}
