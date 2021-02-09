610efe59fd51bf5531e7b4f7e83383bfb28d18a4
ko4evneg <ko4evneg@gmail.com>
2021-02-10 00:18:15 +0300

    Ch06 ex 07

---
+++ src/ch06_accesscontrol/exercise7/Exercise7.java
@@ -0,0 +1,9 @@
+package ch06_accesscontrol.exercise7;
+
+import ch06_accesscontrol.exercise7.access.Widget;
+
+public class Exercise7 {
+    public static void main(String[] args) {
+        Widget w = new Widget();
+    }
+}
---
+++ src/ch06_accesscontrol/exercise7/access/Widget.java
@@ -0,0 +1,13 @@
+package ch06_accesscontrol.exercise7.access;
+
+import static thinkinginjava.Utils.print;
+
+public class Widget {
+    static void printMessage(){
+        print("Widget");
+    }
+
+    void printMsg(){
+        print("Wdgt");
+    }
+}
