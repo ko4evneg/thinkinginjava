35fcf4d56ee59849264d3ad890a312b67d0a54a8
ko4evneg <ko4evneg@gmail.com>
2021-02-21 16:32:36 +0300

    Ch09 Ex 17

---
+++ src/ch09_interfaces/exercise17/Exercise17.java
@@ -0,0 +1,16 @@
+package ch09_interfaces.exercise17;
+//Exercise 17: (2) Prove that the fields in an interface are implicitly static and final.
+
+import static thinkinginjava.Utils.print;
+
+interface StaticAndFinal {
+    int i = 52;
+}
+
+public class Exercise17 implements StaticAndFinal{
+    public static void main(String[] args) {
+        print(i);
+        //can't:
+        //i++;
+    }
+}
