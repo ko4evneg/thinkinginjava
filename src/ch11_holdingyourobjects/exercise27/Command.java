5e1d70ebaea314fe6f9d9c5ddc2c2089ca1d4fd7
ko4evneg <ko4evneg@gmail.com>
2021-03-14 13:52:30 +0300

    Ch 11 ex 27

---
+++ src/ch11_holdingyourobjects/exercise27/Command.java
@@ -0,0 +1,42 @@
+package ch11_holdingyourobjects.exercise27;
+//Exercise 27: (2) Write a class called Command that contains a String and has a method operation( ) that displays the
+//String. Write a second class with a method that fills a Queue with Command objects and returns it. Pass the filled
+//Queue to a method in a third class that consumes the objects in the Queue and calls their operation( ) methods.
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+import static thinkinginjava.Utils.print;
+
+public class Command {
+    private String string;
+
+    public Command(String string) {
+        this.string = string;
+    }
+
+    public void operation() {
+        print(string);
+    }
+}
+
+class QueueUtils {
+    public static Queue<Command> fillQueue() {
+        Queue<Command> queue = new LinkedList<>();
+        queue.offer(new Command("go!"));
+        queue.offer(new Command("stay!"));
+        queue.offer(new Command("fire!"));
+        queue.offer(new Command("down!"));
+        queue.offer(new Command("run!"));
+        queue.offer(new Command("climb!"));
+        return queue;
+    }
+}
+
+class Init {
+    public static void main(String[] args) {
+        for (Command command : QueueUtils.fillQueue()) {
+            command.operation();
+        }
+    }
+}
