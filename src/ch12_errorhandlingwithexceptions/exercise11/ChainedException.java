16b12cd6d512aa5b945255083faa502fd18fb8f6
ko4evneg <ko4evneg@gmail.com>
2021-03-20 23:19:21 +0300

    Ch 12 ex 11

--- src/ch12_errorhandlingwithexceptions/exercise11/ChainedException.java
+++ src/ch12_errorhandlingwithexceptions/exercise11/ChainedException.java
@@ -1,16 +1,17 @@
-package ch12_errorhandlingwithexceptions.exercise10;
+package ch12_errorhandlingwithexceptions.exercise11;
 //Exercise 10: (2) Create a class with two methods, f( ) and g( ). In g( ), throw an exception of a new type that you
 //define. In f( ), call g( ), catch its exception and, in the catch clause, throw a different exception (of a second
 //type that you define). Test your code in main( ).
 
+//Exercise 11: (1) Repeat the previous exercise, but inside the catch clause, wrap g( )’s exception in a
+//RuntimeException.
+
 public class ChainedException {
-    public static void f() throws OuterException{
+    public static void f() throws RuntimeException{
         try {
             g();
         } catch (InnerException e) {
-            OuterException oe = new OuterException();
-            oe.initCause(e);
-            throw oe;
+            throw new RuntimeException(e);
         }
     }
 
@@ -24,7 +25,4 @@ public class ChainedException {
 }
 
 class InnerException extends Exception {
-}
-
-class OuterException extends Exception {
 }
