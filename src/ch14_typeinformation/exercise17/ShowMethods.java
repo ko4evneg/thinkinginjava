83d7e219a3a4e0ed476ae3e7326b86d6af88e6c1
ko4evneg <ko4evneg@gmail.com>
2021-04-13 00:25:08 +0300

    Ch14 ex17

---
+++ src/ch14_typeinformation/exercise17/ShowMethods.java
@@ -0,0 +1,53 @@
+package ch14_typeinformation.exercise17;
+
+import java.lang.reflect.*;
+import java.util.regex.Pattern;
+import static thinkinginjava.Utils.print;
+//Exercise 17: (2) Modify the regular expression in ShowMethods.java to additionally strip off the keywords
+//native and final (hint: use the OR operator’|’)-
+
+public class ShowMethods {
+    private static String usage =
+            "usage:\n" +
+                    "ShowMethods qualified.class.name\n" +
+                    "To show all methods in class or:\n" +
+                    "ShowMethods qualified.class.name word\n" +
+                    "To search for methods involving ‘word’";
+    private static Pattern p = Pattern.compile("((\\w+\\.)|native |final )");
+    public int test = 3;
+    public static void testMethodName(){}
+    public static void main(String[] args) {
+        String className = "ch14_typeinformation.exercise17.ShowMethods";
+        String matchedName = "";
+        int lines = 0;
+        try {
+            Class<?> c = Class.forName(className);
+            Method[] methods = c.getMethods();
+            Constructor[] ctors = c.getConstructors();
+            Field[] fields = c.getFields();
+            if (matchedName.isEmpty()) {
+                for (Method method : methods)
+                    print(
+                            p.matcher(method.toString()).replaceAll(""));
+                for (Constructor ctor : ctors)
+                    print(p.matcher(ctor.toString()).replaceAll(""));
+                lines = methods.length + ctors.length;
+            } else {
+                for (Method method : methods)
+                    if (method.toString().indexOf(matchedName) != -1) {
+                        print(
+                                p.matcher(method.toString()).replaceAll(""));
+                        lines++;
+                    }
+                for (Constructor ctor : ctors)
+                    if (ctor.toString().indexOf(matchedName) != -1) {
+                        print(p.matcher(
+                                ctor.toString()).replaceAll(""));
+                        lines++;
+                    }
+            }
+        } catch (ClassNotFoundException e) {
+            print("No such class: " + e);
+        }
+    }
+}
